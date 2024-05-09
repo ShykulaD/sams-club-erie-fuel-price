6fe3fe48478cd6dd64b18fd86dd5a5fbb8c12eb3
ShykulaD <de.shykula@gmail.com>
2024-05-09 15:55:05 -0400

    write price to file

--- src/test/java/selenide/tests/ErieFuelParseTest.java
+++ src/test/java/selenide/tests/ErieFuelParseTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Tag;
 import selenide.tests.pages.ErieSamsClubPage;
 import selenide.tests.testsuites.TestSuiteConst;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 
 @Tag(TestSuiteConst.FuelSuite)
@@ -12,11 +15,14 @@ public class ErieFuelParseTest {
     private ErieSamsClubPage erieSamsClubPage = new ErieSamsClubPage();
 
     @Test
-    public void unleadedParseTest() {
+    public void unleadedParseTest() throws FileNotFoundException, UnsupportedEncodingException {
         LocalDate currentDate = LocalDate.now();
 
         erieSamsClubPage.open();
         String unleadedPrice = erieSamsClubPage.getUnleadedPrice();
-        System.out.println("Now local USA time in Erie,PA is " + currentDate + " and Unleaded gas price is " + unleadedPrice);
+
+        PrintWriter writer = new PrintWriter("gasPrice.txt", "UTF-8");
+        writer.println("Now local USA time in Erie,PA is " + currentDate + " and Unleaded gas price is " + unleadedPrice);
+        writer.close();
     }
 }
