b1ccde5eaaf4e35a41f8aefef30fde521ae7b6ee
ShykulaD <de.shykula@gmail.com>
2025-11-14 11:48:44 -0500

    Fix test locator

--- src/test/java/selenide/tests/pages/ErieSamsClubPage.java
+++ src/test/java/selenide/tests/pages/ErieSamsClubPage.java
@@ -8,12 +8,12 @@ import selenide.tests.pages.common.CommonPage;
 import java.util.Objects;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class ErieSamsClubPage extends CommonPage {
     public static final String PAGE_URL = Configuration.baseUrl + "club/erie-pa-sams-club/6675?GAS=aL";
 
-    public SelenideElement unleadedGas = $("[aria-label*='Unleaded']");
+    public SelenideElement unleadedGas = $x("//div[contains(text(), 'Unleaded')]/..//span");
 
     public ErieSamsClubPage open() {
         Selenide.open(PAGE_URL);
@@ -27,8 +27,11 @@ public class ErieSamsClubPage extends CommonPage {
     }
 
     public String getUnleadedPrice() {
-        return Objects.requireNonNull(Objects.requireNonNull(unleadedGas.shouldBe(visible.because("Unleaded gas price should be present on the page"))
-                .getAttribute("aria-label")).substring(9));
+        return Objects.requireNonNull(
+                unleadedGas
+                        .shouldBe(visible.because("Unleaded gas price should be present on the page"))
+                        .getText()
+        ).substring(9);
     }
 
 }
